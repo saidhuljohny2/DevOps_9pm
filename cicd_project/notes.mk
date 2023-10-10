1dee99b378e919172ea8c4e4b58439af5fa80e1a
saidhuljohny@gmail.com <jkskjdhjksdf>
2023-10-10 08:30:50 +0530

    added

--- cicd_project/notes.mk
+++ cicd_project/notes.mk
@@ -1,3 +1,6 @@
+        # create VM with good configuration
+project-server 
+
         # 1.  Jenkins Installation on Ubuntu ###
 sudo su - ubuntu
 sudo apt-get update
@@ -15,6 +18,8 @@ sudo systemctl enable jenkins       //Enable the Jenkins service to start at boo
 sudo systemctl start jenkins        //Start Jenkins as a service
 systemctl status jenkins
 
+jenkins => setup => random agent, enable proxy server
+
         # 2. Ansible Installation on Ubuntu #
 sudo su - ubuntu
 $ sudo usermod -aG sudo ubuntu
@@ -23,7 +28,7 @@ $ sudo apt update
 $ sudo apt install ansible-core
 $ ansible --version
 
-        # 3. integrate ansible with Jenkins and install Docker on ansible-server #
+        # 3. integrate ansible with Jenkins #
 
 ssh-keygen
 cat /home/ubuntu/.ssh/id_rsa.pub ==> copy key
@@ -46,25 +51,28 @@ $ sudo usermod -aG docker ubuntu
 $ id ubuntu
 
         ## 5. add a node ##
-add node : ansible-node
+add node : ansible
     path => /opt/docker
     run the command in shell
-    connected
+    => connected
 
         ## 6. Create Ansible Playbook to Create Docker Image and Copy Image to DockerHub ##
 
 add hosts in ansible : sudo nano /etc/ansible/hosts
    => copy and paste the internal ip 
 
-ssh-copy-id 10.154.0.26
+ssh-copy-id 10.154.0.26 (verify)
 cd /opt/docker/
 nano cafe-app.yml
 ls
 sudo docker login ==> enter login details 
 ansible-playbook cafe-app.yml --check
 ansible-playbook cafe-app.yml
-docker images
+- check folder created ?
+- check => docker images
+- check in docker hub
 
+##### CREATE CI JOB:
 
 go to jenkins => new item => ci_job_cafeapp ==> freestyle project
 git => https://github.com/saidhuljohny2/Cafe-App.git
@@ -75,10 +83,13 @@ build now => check console output
 
           # 7.  create GKE cluster in GCP #
 go to kubernates => create a single node cluster
+
 and then
-# Install the gcloud CLI 
-- https://cloud.google.com/sdk/docs/install#deb
-- https://cloud.google.com/kubernetes-engine/docs/how-to/cluster-access-for-kubectl#apt_1
+
+# Install the gcloud CLI and kubectl, plugin
+on root 
+- google cli : https://cloud.google.com/sdk/docs/install#deb
+- kubectl & plugin : https://cloud.google.com/kubernetes-engine/docs/how-to/cluster-access-for-kubectl#apt_1
 
 => Connect to the cluster from command
 => kubectl get nodes
@@ -99,9 +110,9 @@ PermitRootLogin yes
 systemctl restart sshd
 passwd root
 at ubuntu :
-$ ssh-copy-id root@10.0.0.6
+$ ssh-copy-id root@
 $ ssh 'root@10.0.0.6'
-$ mv cafe-app.yml create-image-cafe-app.yml 
+ 
 $ nano kubernetes-deploy.yml
 $ ansible-playbook kubernetes-deploy.yml
   => check in kube server for => kubectl get all => get the external ip to see website running 
